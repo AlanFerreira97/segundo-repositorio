b6724b02e13b1b53c1a605cc397269c26612c48a
AlanFerreira97 <alan-_-2007@hotmail.com>
2017-04-17 20:24:03 -0300

    first commit

---
+++ Impar ou Par.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int n1,num;
+	
+	printf("informe o numero:");
+	scanf("%d",&n1);
+	
+	num=n1%2;
+	
+	if(num==0)
+		printf("o numero e par.\n");
+		
+	else{
+		if(num!=10)
+		printf("o numero e impar.\n");
+			
+
+}	
+system("pause");
+
+}
+
+
+
+
